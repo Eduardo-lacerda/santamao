26771106e325bcfb3a68b8e86d3324b2c96e6118
Eduardo-lacerda <lacerda.eduardo2505@gmail.com>
2021-06-14 18:47:20 -0300

    deploy

--- backend/api/models/orcamentoModel.js
+++ backend/api/models/orcamentoModel.js
@@ -2,28 +2,23 @@ module.exports = (mongoose) => {
   const orcamentoSchema = new mongoose.Schema({
       nome: {
           type: String,
-          required: true,
           minlength: 5,
           maxlength: 255,
       },
       email: {
           type: String,
-          required: true,
           unique: false,
           minlength: 5,
           maxlength: 255,
       },
       bairro: {
         type: String,
-        required: true,
       },
       telefone: {
         type: String,
-        required: true,
       },
       comentario: {
         type: String,
-        required: true,
       },
       limpeza: {
         type: [{
